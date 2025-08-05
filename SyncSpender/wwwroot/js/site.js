45524874cd46f72c288e52a48669203a367a2156
JannatulNancy <jnnancy345@gmail.com>
2025-08-05 23:59:08 +0600

    Commit message: Design pass: Category form, headings, breadcrumb, assets and site icon.
    Version tag:
    v1.0-design-pass

--- SyncSpender/wwwroot/js/site.js
+++ SyncSpender/wwwroot/js/site.js
@@ -2,3 +2,17 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+{
+    "icons": [
+        {
+            "src": "/favicon-192x192.png",
+            "sizes": "192x192",
+            "type": "image/png"
+        },
+        {
+            "src": "/favicon-512x512.png",
+            "sizes": "512x512",
+            "type": "image/png"
+        }
+    ]
+}
