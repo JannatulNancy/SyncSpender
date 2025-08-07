992f4c38acb37dc8f4a04d2cc1eb0dc3d4b90843
JannatulNancy <jnnancy345@gmail.com>
2025-08-07 12:21:26 +0600

    [model] Make Category property nullable to support uncategorized transactions

--- SyncSpender/Models/Transaction.cs
+++ SyncSpender/Models/Transaction.cs
@@ -11,12 +11,12 @@ namespace SyncSpender.Models
         [Column(TypeName = "nvarchar(100)")]
         public string Title { get; set; }
         public decimal Amount { get; set; }
-        public DateTime Date { get; set; }
+        public DateTime Date { get; set; } = DateTime.Now;
 
         [Column(TypeName = "nvarchar(100)")]
         public string? Note { get; set; }
         public int CategoryId { get; set; }
-        public Category Category { get; set; }
+        public Category? Category { get; set; }
         // Foreign key for the user
        
     }
